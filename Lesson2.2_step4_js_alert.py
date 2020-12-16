dfa69e2d7012392e606642ff6ccd85799eb46fd6
Ruslan Sadykov <interligo@yandex.ru>
2020-12-16 13:20:55 +0300

    Delete Lesson2.2_step4_js_alert.py

--- Lesson2.2_step4_js_alert.py
+++
@@ -1,6 +0,0 @@
-from selenium import webdriver
-
-
-browser = webdriver.Chrome()
-# execute_script(javascript_code)
-browser.execute_script("document.title='Script executing';alert('Robots at work');")
